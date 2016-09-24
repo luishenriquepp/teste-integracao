63ab70a439b842dfe03e5c2efed1fbc75baa47e9
luishenriquepp <peppyno@gmail.com>
2016-09-24 00:14:49 -0300

    Inclusao do angular-cli

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor/globals';
+
+export class TestePage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
